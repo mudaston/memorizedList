deefb8991f0d1d4e93b36f188d771e8d00278535
ren vasco <ren.vasco@kiv.dev>
2024-04-04 22:53:49 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,51 +1,76 @@
-import { useState, useEffect, useMemo, useRef } from 'react'
+import {
+	useState,
+	useRef,
+	useLayoutEffect,
+	useEffect,
+	useCallback,
+} from "react";
 
-import photos from './photos.json'
+// import photos from "./photos.json";
 
-import './App.css'
+import "./App.css";
 
 const Row = ({ index, style }) => {
-	const [value, setValue] = useState('State')
-	const [counter, setCounter] = useState(0)
-	const pussyRef = useRef(null)
+	const [value, setValue] = useState("State");
+	// const [counter, setCounter] = useState(0);
 
-	useEffect(() => {
-		setInterval(() => {
-			setCounter((prevState) => prevState + 1)
-			setValue(Math.random())
-		}, 1000)
-	}, [])
-
-	useEffect(() => {
-		if (!pussyRef.current) return
-
-		pussyRef.current.innerHTML += 'PUSSY'
-	}, [value, counter])
+	// useEffect(() => {
+	// 	setInterval(() => {
+	// 		setCounter((prevState) => prevState + 1);
+	// 		setValue(Math.random());
+	// 	}, 1000);
+	// }, []);
 
 	return (
-		<div style={{ ...style, display: 'flex', flexWrap: 'wrap', gap: 10 }}>
-			<div>id: {photos[index].id}</div>
-			<div>title: {photos[index].title}</div>
+		<div style={{ ...style, display: "flex", flexWrap: "wrap", gap: 10 }}>
+			<div>id: {index}</div>
+			{/* <div>title: {photos[index].title}</div>
 			<div>url: {photos[index].url}</div>
 			<div>: {photos[index].albumId}</div>
-			<div>: {photos[index].thumbnailUrl}</div>
+			<div>: {photos[index].thumbnailUrl}</div> */}
 			<input
 				type="text"
 				value={value}
 				onChange={(e) => setValue(e.target.value)}
 			/>
-			<div>time: {counter}</div>
-			<div ref={pussyRef} />
+			{/* <div>time: {counter}</div> */}
 		</div>
-	)
-}
+	);
+};
 
 function App() {
+	const [list, setList] = useState([]);
+	const [fullList, setFullList] = useState(() => {
+		const list = [];
+		for (let i = 0; i < 9999; i++) list.push(i);
+		return list;
+	});
+
 	return (
-		<VirtualizedList height={550} itemCount={5000} itemSize={21} offset={30}>
-			{({ index, style }) => <Row key={index} index={index} style={style} />}
-		</VirtualizedList>
-	)
+		<div style={{ display: "flex", gap: "100px" }}>
+			<div style={{ width: "500px" }}>
+				<VirtualizedList
+					height={860}
+					itemCount={100}
+					itemSize={176}
+					pageSize={5}
+					onNewPage={({ startIndex, endIndex }) => {
+						console.log(startIndex, endIndex);
+						setList(fullList.slice(startIndex, endIndex + 1));
+					}}
+				>
+					{({ index, style }) => (
+						<Row key={index} index={index} style={style} />
+					)}
+				</VirtualizedList>
+			</div>
+			<div>
+				{list.map((i) => (
+					<div key={i}>{i}</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 const VirtualizedList = ({
@@ -53,73 +78,170 @@ const VirtualizedList = ({
 	itemSize,
 	itemCount,
 	children,
-	offset = 0,
+	onScroll,
+	onNewPage,
+	pageSize,
 }) => {
-	const [indexes, setIndexes] = useState([])
+	const itemsInView = useRef(Math.round(height / itemSize));
+	const offset = useRef(
+		pageSize - itemsInView.current <= 0 ? 0 : pageSize - itemsInView.current
+	);
+	const calculatedPageSize = useRef(itemsInView.current + offset.current);
 
-	const parentRef = useRef(null)
+	const [indexes, setIndexes] = useState([]);
+	const [startIndexPage, setStartIndexPage] = useState(0);
+	const [endIndexPage, setEndIndexPage] = useState(
+		() => calculatedPageSize.current
+	);
+
+	// fix this moment and check if it render item count 5 page size 10
+	console.log("awdawd", Math.floor((itemSize * itemCount) / itemSize) - 1);
+
+	// console.log({ startIndexPage, endIndexPage });
+
+	const parentRef = useRef(null);
+
+	const getItemsInView = useCallback(
+		(e) => {
+			const target = e?.target;
+			const scrollTop = target?.scrollTop || 0;
+			let startIndex = Math.floor(scrollTop / itemSize);
+			let endIndex =
+				itemSize * itemCount < height
+					? Math.floor((itemSize * itemCount) / itemSize)
+					: Math.floor((scrollTop + height) / itemSize);
+
+			// console.log(height);
+			// console.log(itemSize * itemCount);
+			// console.log(Math.floor((itemSize * itemCount) / itemSize));
+			// console.log(Math.floor((scrollTop + height) / itemSize));
+
+			if (endIndex > itemCount) return;
+
+			endIndex +=
+				itemCount - endIndex < offset.current
+					? itemCount - endIndex
+					: offset.current;
+
+			// console.log({ startIndex, endIndex });
+
+			// scroll up
+			if (startIndex < startIndexPage && startIndex !== 0) {
+				console.log("scroll up");
+				setStartIndexPage((prev) => prev - calculatedPageSize.current);
+				setEndIndexPage((prev) => {
+					if (
+						itemCount % calculatedPageSize.current !== 0 &&
+						prev === itemCount
+					) {
+						return prev - (prev - startIndexPage);
+					}
+
+					return prev - calculatedPageSize.current;
+				});
+			}
+
+			// scroll down
+			if (endIndexPage - startIndex <= 0) {
+				console.log("scroll down");
+				setStartIndexPage((prev) => prev + calculatedPageSize.current);
+				setEndIndexPage((prev) => {
+					const newIndex = prev + calculatedPageSize.current;
+					if (newIndex > itemCount) return prev + itemCount - prev;
+					if (
+						(itemCount - newIndex) * itemSize <=
+						itemsInView.current * itemSize
+					) {
+						return newIndex + (itemCount - newIndex);
+					}
+					return newIndex;
+				});
+			}
+
+			onScroll?.({ startIndex, endIndex });
+
+			let array = [];
+
+			for (let i = startIndex; i <= endIndex; i++) {
+				if (i === itemCount) break;
+				array.push(i);
+			}
+			setIndexes(array);
+		},
+		[itemSize, height, itemCount, startIndexPage, endIndexPage]
+	);
+
+	useLayoutEffect(() => {
+		// const parent = parentRef.current;
+		// if (!parent) return;
+
+		getItemsInView();
+
+		// parent.scrollTo(1, 1);
+		// parent.scrollTo(0, 0);
+	}, []);
+
+	useLayoutEffect(() => {
+		const parent = parentRef.current;
+		if (!parent) return;
+
+		parent.removeEventListener("scroll", getItemsInView, true);
+		parent.addEventListener("scroll", getItemsInView, true);
+
+		return () => {
+			parent.removeEventListener("scroll", getItemsInView, true);
+		};
+	}, [parentRef, getItemsInView, itemSize, height, itemCount]);
 
 	useEffect(() => {
-		const scrollHandler = (e) => {
-			const { target } = e
-			const scrollTop = target.scrollTop
-			let startIndex = Math.floor(scrollTop / itemSize)
-			let endIndex = Math.floor((scrollTop + height) / itemSize)
+		// console.log({ endIndexPage });
 
-			if (startIndex > 0) startIndex -= 1
-			if (startIndex > offset - 1) startIndex -= offset - 1
+		// let endIndex = endIndexPage === itemCount ? endIndexPage : endIndexPage - 1;
+		let endIndex =
+			endIndexPage === itemCount ? endIndexPage - 1 : endIndexPage - 1;
 
-			if (endIndex + offset < itemCount) endIndex += offset
+		// console.log({ startIndexPage, endIndexPage });
 
-			if (endIndex > itemCount) return
+		if (itemCount === 0) return;
+		// if (pageSize > itemCount) {
+		// 	console.log({ pageSize });
+		// 	console.log({ itemCount });
+		// }
 
-			let array = []
+		onNewPage?.({
+			startIndex: startIndexPage,
+			endIndex,
+		});
+	}, [endIndexPage, startIndexPage]);
 
-			for (let i = startIndex; i < endIndex; i++) array.push(i)
-
-			setIndexes(array)
-		}
-
-		parentRef.current.addEventListener('scroll', scrollHandler, true)
-	}, [parentRef])
-
-	useEffect(() => {
-		const lastIndex = Math.floor(height / itemSize)
-
-		let array = []
-
-		for (let i = 0; i < lastIndex; i++) array.push(i)
-
-		setIndexes(array)
-	}, [])
+	// console.log({ startIndexPage, endIndexPage });
 
 	return (
 		<div
 			ref={parentRef}
 			style={{
-				position: 'relative',
+				position: "relative",
 				height,
-				overflow: 'auto',
-				willChange: 'transform',
+				overflow: "auto",
+				willChange: "transform",
 			}}
 		>
 			<div style={{ height: itemCount * itemSize }}>
 				{indexes.map((index) =>
 					children({
 						style: {
-							position: 'absolute',
+							position: "absolute",
 							left: 0,
-							// top: index * itemSize,
 							transform: `translateY(${index * itemSize}px)`,
 							height: itemSize,
-							width: '100%',
+							width: "100%",
 						},
 						index,
 					})
 				)}
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default App
+export default App;
